b07fb71f83ea5f218c9938e28222681865b38d0f
TANMOY PAL <tanmoypal30102004@gmail.com>
2026-02-15 21:57:10 +0530

    cleanup

--- backend/src/middleware/auth.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-interface AuthRequest extends Request {
-    user?: any;
-}
-export declare const protect: (req: AuthRequest, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-export declare const authorize: (...roles: string[]) => (req: AuthRequest, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-export {};
-//# sourceMappingURL=auth.d.ts.map
